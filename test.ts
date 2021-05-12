2eb5679ce92647056b5abcdc5947c196ea87c727
reifab <72970313+reifab@users.noreply.github.com>
2021-05-12 14:54:29 +0200

    tests ergänzt

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
-// Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+Farbsensor.start(TCS34725_AGAIN.EMPFINDLICHKEIT_60X)
+basic.forever(function () {
+    serial.writeValue("r", Farbsensor.getSensorData(RGB.ROT))
+    serial.writeValue("g", Farbsensor.getSensorData(RGB.GRUEN))
+    serial.writeValue("b", Farbsensor.getSensorData(RGB.BLAU))
+    serial.writeValue("c", Farbsensor.getSensorData(RGB.ALLE_FARBEN))
+})
